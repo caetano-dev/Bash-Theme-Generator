3249938fa13dce23b69efdf649eaa566a2202979
drull1000 <pcaetanop@protonmail.com>
2021-07-26 20:04:05 -0300

    Creating components

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Global.css";
+import styles from "../styles/Navbar.module.css";
+function Navbar() {
+  return (
+    <div className={styles.Navbar}>
+      <h1>Console Color Generator</h1>
+    </div>
+  );
+}
+
+export default Navbar;
